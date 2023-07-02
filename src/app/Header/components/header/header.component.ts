aade9762d5553269f17b25e68ddc669f06caa9b3
v_manijashvili <v@leri.ge>
2023-07-02 13:05:47 +0400

    test

--- src/app/Header/components/header/header.component.ts
+++ src/app/Header/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-    isHidden: boolean = false;
-  
-    isMenuOpened: boolean = false;
+  isHidden: boolean = false;
 
-  toggleMenu(){
-    this.isMenuOpened = !this.isMenuOpened;}
+  isMenuOpened: boolean = false;
+  activeSubMenu = 'menu1';
+
+  setSubMenu = (menuId: string) => this.activeSubMenu = menuId;
+
+
+  toggleMenu = () => this.isMenuOpened = !this.isMenuOpened;
 }
