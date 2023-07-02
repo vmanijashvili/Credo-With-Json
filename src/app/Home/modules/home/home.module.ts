5bf64c8581e13d41054456cfca8c7625ca084e59
kakha chaduneli <chadunelikakha@gmail.com>
2023-07-02 12:25:12 +0400

    javascript

---
+++ src/app/Home/modules/home/home.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from '../../components/home/home.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+  declarations: [
+    HomeComponent
+  ],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    NgbModule,
+  ]
+})
+export class HomeModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { HomeComponent } from './Home/components/home/home.component';
 import { HeaderComponent } from './Header/components/header/header.component';
 import { RightNavContainerComponent } from './Right-nav-container/components/right-nav-container/right-nav-container.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { FooterComponent } from './Footer/components/footer/footer.component';
 
+import { FooterComponent } from './Footer/components/footer/footer.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -18,14 +17,15 @@ import { FooterComponent } from './Footer/components/footer/footer.component';
     HeaderComponent,
     
     RightNavContainerComponent,
-    HomeComponent,
     FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    NgbModule,
+   
+    RouterModule,
+  
     
   ],
   providers: [],
